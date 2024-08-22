719202603a98e689ef9b86ea0f1c7e667f93da9e
subhashprasad2001@gmail.com <{96110162+subhashdippu@users.noreply.github.com}+{subhashdippu}@users.noreply.github.com>
2024-08-22 18:56:10 +0530

    user signin

--- router/auth.js
+++ router/auth.js
@@ -9,29 +9,48 @@ app.get("/", (req, res) => {
   res.send("Hello this is the home page");
 });
 
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
   const { name, email, phone, work, password, cpassword } = req.body;
   if (!name || !email || !phone || !work || !password || !cpassword) {
     return res.status(422).json({ error: "Please fill the all data" });
   }
-  User.findOne({ email: email })
-    .then((userExist) => {
-      if (userExist) {
-        return res.status(422).json({ error: "Email already exist" });
-      }
-      const user = new User({ name, email, phone, work, password, cpassword });
 
-      user
-        .save()
-        .then(() => {
-          res.status(201).json({ message: "User registered successfully" });
-        })
-        .catch((err) =>
-          res.status(500).json({ message: "Failed registration" })
-        );
-    })
-    .catch((err) => {
-      console.log(err);
+  try {
+    const userExist = await User.findOne({ email: email });
+    if (userExist) {
+      return res.status(422).json({ error: "Email already exist" });
+    }
+    const user = new User({
+      name,
+      email,
+      phone,
+      work,
+      password,
+      cpassword,
     });
+
+    await user.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (err) {
+    console.log(err);
+  }
 });
+
+app.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(422).json({ error: "Please fill the all data" });
+  }
+  try {
+    const userLogin = await User.findOne({ email: email });
+    if (!userLogin) {
+      res.status(422).json({ error: "User not exist" });
+    } else {
+      res.status(422).json({ message: "User signin successfully" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 module.exports = app;
