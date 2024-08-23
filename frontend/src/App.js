fabae8036990f5fe5d32c5e78f93a320a5e50ec6
subhashprasad2001@gmail.com <{96110162+subhashdippu@users.noreply.github.com}+{subhashdippu}@users.noreply.github.com>
2024-08-23 17:00:35 +0530

    adding navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return <div>Subhash Prasad</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default App;
