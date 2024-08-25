fb363de019b9c466b53e4c7d9c5a7a0452faabc9
subhashprasad2001@gmail.com <{96110162+subhashdippu@users.noreply.github.com}+{subhashdippu}@users.noreply.github.com>
2024-08-25 18:33:23 +0530

    saving data in state

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,24 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SignUp = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    work: "",
+    password: "",
+    cpassword: "",
+  });
+  let value, name;
+  const handleChange = (e) => {
+    name = e.target.name;
+    value = e.target.value;
+
+    setUser({ ...user, [name]: value });
+  };
+
   return (
     <div className="container">
       <form>
         <div className="mb-3">
-          <label htmlFor="work" className="form-label">
+          <label htmlFor="name" className="form-label">
             Your Name
           </label>
-          <input type="text" className="form-control" id="work" />
+          <input
+            type="text"
+            className="form-control"
+            id="work"
+            name="name"
+            value={user.name}
+            onChange={handleChange}
+          />
         </div>
+
         <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">
+          <label htmlFor="email" className="form-label">
             Email address
           </label>
           <input
             type="email"
             className="form-control"
-            id="exampleInputEmail1"
+            id="email"
             aria-describedby="emailHelp"
+            name="email"
+            value={user.email}
+            onChange={handleChange}
           />
           <div id="emailHelp" className="form-text"></div>
         </div>
@@ -27,28 +54,56 @@ const SignUp = () => {
           <label htmlFor="phone" className="form-label">
             Your Phone
           </label>
-          <input type="number" className="form-control" id="phone" />
+          <input
+            type="number"
+            className="form-control"
+            id="phone"
+            name="phone"
+            value={user.phone}
+            onChange={handleChange}
+          />
         </div>
 
         <div className="mb-3">
           <label htmlFor="work" className="form-label">
             Your role
           </label>
-          <input type="text" className="form-control" id="work" />
+          <input
+            type="text"
+            className="form-control"
+            id="work"
+            name="work"
+            value={user.role}
+            onChange={handleChange}
+          />
         </div>
 
         <div className="mb-3">
           <label htmlFor="password" className="form-label">
             Password
           </label>
-          <input type="password" className="form-control" id="password" />
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            name="password"
+            value={user.password}
+            onChange={handleChange}
+          />
         </div>
 
         <div className="mb-3">
           <label htmlFor="cpassword" className="form-label">
             Confirm Password
           </label>
-          <input type="password" className="form-control" id="cpassword" />
+          <input
+            type="password"
+            className="form-control"
+            id="cpassword"
+            name="cpassword"
+            value={user.cpassword}
+            onChange={handleChange}
+          />
         </div>
 
         <button type="submit" className="btn btn-primary">
